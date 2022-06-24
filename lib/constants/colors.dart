9247d4dd75a61d209a568346e4c9cd83a70b02c9
Nguyễn Dũng <dpron12345@gmail.com>
2022-06-24 13:28:25 +0700

    Create colors.dart

---
+++ lib/constants/colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF2ecc71);
+const kSecondaryColor = Color(0xFFCECECE);
+
+const fieldColor = MaterialColor(
+  0xFF37dd7e,
+  <int, Color>{
+    50: Color(0xFF68ffa8),
+    100: Color(0xFF55f499),
+    200: Color(0xFF4cf092),
+    300: Color(0xFF43e889),
+    400: Color(0xFF37dd7e),
+    500: Color(0xFF2ecc71),
+    600: Color(0xFF27c068),
+    700: Color(0xFF23b561),
+    800: Color(0xFF1da858),
+    900: Color(0xFF179c50),
+  },
+);
