da82bc583b6ed7ddefcd065ba7ccec27b0f838f4
Nguyễn Dũng <dpron12345@gmail.com>
2022-06-28 17:16:32 +0700

    Update routes.dart

--- lib/routes.dart
+++ lib/routes.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:sfbms_mobile/views/screens/home/home_screen.dart';
 import 'package:sfbms_mobile/views/screens/login/login_screen.dart';
 import 'package:sfbms_mobile/views/screens/search/search_screen.dart';
+import 'package:sfbms_mobile/views/screens/settings/settings_screen.dart';
 
 final Map<String, WidgetBuilder> routes = {
   LoginScreen.routeName: (context) => const LoginScreen(),
   HomeScreen.routeName: (context) => const HomeScreen(),
   SearchScreen.routeName: (context) => const SearchScreen(),
+  SettingsScreen.routeName: (context) => const SettingsScreen(),
 };
