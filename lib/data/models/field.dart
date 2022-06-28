6a614c4ea3bd4602ca3264f953b7f0af861c15e8
Nguyễn Dũng <dpron12345@gmail.com>
2022-06-28 18:06:32 +0700

    Create Fields model

--- lib/data/models/field.dart
+++ lib/data/models/field.dart
@@ -1,32 +1,6 @@
 import 'package:sfbms_mobile/data/models/category.dart';
 import 'package:sfbms_mobile/data/models/slot.dart';
 
-class Fields {
-  List<Field>? fields;
-  int? numOfFieldPages;
-
-  Fields({this.fields, this.numOfFieldPages});
-
-  Fields.fromJson(Map<String, dynamic> json) {
-    if (json['fields'] != null) {
-      fields = <Field>[];
-      json['fields'].forEach((v) {
-        fields!.add(Field.fromJson(v));
-      });
-    }
-    numOfFieldPages = json['numOfFieldPages'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    if (fields != null) {
-      data['fields'] = fields!.map((v) => v.toJson()).toList();
-    }
-    data['numOfFieldPages'] = numOfFieldPages;
-    return data;
-  }
-}
-
 class Field {
   int? id;
   String? name;
