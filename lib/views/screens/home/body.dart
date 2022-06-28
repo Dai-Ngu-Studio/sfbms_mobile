27432d547da783bb4001dfff8ce6971800f654cf
Nguyễn Dũng <dpron12345@gmail.com>
2022-06-28 22:28:42 +0700

    Finish Home screen

--- lib/views/screens/home/body.dart
+++ lib/views/screens/home/body.dart
@@ -1,17 +1,96 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
+import 'package:sfbms_mobile/view_model/field_viewmodel.dart';
+import 'package:sfbms_mobile/view_model/user_viewmodel.dart';
 import 'package:sfbms_mobile/views/screens/home/widgets/field_item.dart';
 
-class Body extends StatelessWidget {
+class Body extends StatefulWidget {
   const Body({Key? key}) : super(key: key);
 
   @override
+  State<Body> createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  final refreshController = RefreshController(initialRefresh: false);
+  FieldViewModel? fieldViewModel;
+  UserViewModel? userViewModel;
+  bool _isInit = true;
+
+  @override
+  void didChangeDependencies() {
+    if (_isInit) {
+      fieldViewModel = Provider.of<FieldViewModel>(context);
+      userViewModel = Provider.of<UserViewModel>(context);
+      _onRefresh();
+    }
+    _isInit = false;
+    super.didChangeDependencies();
+  }
+
+  Future<bool?> _onRefresh({bool isRefresh = false}) async {
+    var result = await fieldViewModel!.getFields(
+      idToken: (await userViewModel!.idToken)!,
+      isRefresh: isRefresh,
+    );
+
+    if (result == null) {
+      return null;
+    }
+
+    return true;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: const [
-        FieldItem(),
-        FieldItem(),
-        FieldItem(),
-      ],
+    return Consumer2<UserViewModel, FieldViewModel>(
+      builder: ((context, userVM, fieldVM, _) {
+        return SmartRefresher(
+          enablePullDown: true,
+          enablePullUp: true,
+          controller: refreshController,
+          onRefresh: () async {
+            refreshController.resetNoData();
+            var result = await _onRefresh(isRefresh: true);
+
+            if (result!) {
+              refreshController.refreshCompleted();
+            } else {
+              refreshController.loadFailed();
+            }
+          },
+          onLoading: () async {
+            final result = await _onRefresh();
+
+            if (result == null) {
+              refreshController.loadNoData();
+            } else if (result) {
+              refreshController.loadComplete();
+            } else {
+              refreshController.loadFailed();
+            }
+          },
+          header: const WaterDropHeader(),
+          footer: const ClassicFooter(
+            loadStyle: LoadStyle.ShowWhenLoading,
+            completeDuration: Duration(milliseconds: 500),
+          ),
+          child: ListView.builder(
+            itemCount: fieldVM.fields.data?.fields?.length ?? 0,
+            itemBuilder: (context, index) {
+              if (fieldVM.fields.data == null) {
+                return const Center(child: Text("No fields found!."));
+              }
+              return FieldItem(
+                name: fieldVM.fields.data!.fields![index].name!,
+                imageUrl: fieldVM.fields.data!.fields![index].imageUrl!,
+                availableTime: fieldVM.fields.data!.fields![index].slots?.toList() ?? [],
+              );
+            },
+          ),
+        );
+      }),
     );
   }
 }
--- lib/views/screens/home/widgets/field_item.dart
+++ lib/views/screens/home/widgets/field_item.dart
@@ -1,51 +1,76 @@
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:sfbms_mobile/data/models/booking_time.dart';
+import 'package:sfbms_mobile/data/models/slot.dart';
+import 'package:sfbms_mobile/utils/utils.dart';
 
 class FieldItem extends StatelessWidget {
-  const FieldItem({Key? key}) : super(key: key);
+  const FieldItem({
+    Key? key,
+    required this.name,
+    required this.imageUrl,
+    required this.availableTime,
+  }) : super(key: key);
+
+  final String name;
+  final String imageUrl;
+  final List<Slot> availableTime;
 
   @override
   Widget build(BuildContext context) {
+    final List<BookingTime> bookingTimes = Utils.getBookingTimesBySlots(availableTime);
+
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 18.0, vertical: 9),
-      child: Container(
-        padding: const EdgeInsets.all(12.0),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(14),
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.08),
-              blurRadius: 10,
-              spreadRadius: 5,
-            ),
-          ],
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(14),
-              child: ExtendedImage.network(
-                'https://picsum.photos/id/1/2000/2000',
-                fit: BoxFit.cover,
-                cache: true,
-                enableLoadState: true,
-                height: 300,
-                width: double.infinity,
+      child: InkWell(
+        borderRadius: BorderRadius.circular(14),
+        onTap: () {},
+        child: Container(
+          padding: const EdgeInsets.all(12.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(14),
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.08),
+                blurRadius: 10,
+                spreadRadius: 5,
               ),
-            ),
-            const SizedBox(height: 16),
-            Text(
-              "Soccer Field",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 16),
-            Text(
-              "Available: Morning, Afternoon, Evening",
-              style: TextStyle(fontSize: 18, color: Colors.black54),
-            )
-          ],
+            ],
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(14),
+                child: ExtendedImage.network(
+                  imageUrl,
+                  fit: BoxFit.cover,
+                  cache: true,
+                  enableLoadState: true,
+                  height: 300,
+                  width: double.infinity,
+                ),
+              ),
+              const SizedBox(height: 16),
+              Text(
+                name,
+                style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              bookingTimes.isNotEmpty
+                  ? Container(
+                      margin: const EdgeInsets.only(top: 16),
+                      child: Text(
+                        "Available: ${bookingTimes.map((bookingTime) {
+                          return toBeginningOfSentenceCase(bookingTime.name.toLowerCase())!;
+                        }).join(", ")}",
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                    )
+                  : const SizedBox.shrink()
+            ],
+          ),
         ),
       ),
     );
