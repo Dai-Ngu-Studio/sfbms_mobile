e6517ea64dd172d487d3e443ccc46dd04cf9397a
Nguyễn Dũng <dpron12345@gmail.com>
2022-06-28 22:27:42 +0700

    Add enum BookingTime

---
+++ lib/data/models/booking_time.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: constant_identifier_names
+enum BookingTime {
+  // 06:00 - 10:00
+  MORNING,
+  // 10:00 - 14:00
+  NOON,
+  // 14:00 - 18:00
+  AFTERNOON,
+  // 18:00 - 22:00
+  EVENING,
+  // 22:00 - 02:00
+  NIGHT,
+  // 02:00 - 06:00
+  MIDNIGHT,
+}
