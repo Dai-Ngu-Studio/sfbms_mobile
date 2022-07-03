9491d4073a51b75317d6a2c1998a1165660a9b35
Nguyễn Dũng <dpron12345@gmail.com>
2022-07-04 01:02:08 +0700

    fix missing param

--- lib/data/repository/interface/booking_repository.dart
+++ lib/data/repository/interface/booking_repository.dart
@@ -2,6 +2,10 @@ import 'package:sfbms_mobile/data/models/booking.dart';
 import 'package:sfbms_mobile/data/models/bookings.dart';
 
 abstract class BookingRepository {
-  Future<Bookings> getBookings({required String idToken});
-  Future<Booking> getBookingByID({required String idToken, required String bookingID});
+  Future<Bookings> getBookings({required String idToken, String? odataSegment});
+  Future<Booking> getBookingByID({
+    required String idToken,
+    required String bookingID,
+    String? odataSegment,
+  });
 }
